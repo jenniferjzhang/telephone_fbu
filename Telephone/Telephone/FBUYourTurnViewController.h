a9c4e3eb22e218877cd7d5bacbb169c44b9e9111
Jennifer Zhang <jenniferjzhang@fb.com>
2014-07-08 15:37:43 -0700

    Added new view controller for Your Turn and some other features

---
+++ Telephone/Telephone/FBUYourTurnViewController.h
@@ -0,0 +1,17 @@
+//
+//  FBUYourTurnViewController.h
+//  Telephone
+//
+//  Created by Jennifer Zhang on 7/8/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBUYourTurnViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *viewDrawingButton;
+@property (weak, nonatomic) IBOutlet UILabel *yourTurnLabel;
+
+
+@end
---
+++ Telephone/Telephone/FBUYourTurnViewController.m
@@ -0,0 +1,13 @@
+//
+//  FBUYourTurnViewController.m
+//  Telephone
+//
+//  Created by Jennifer Zhang on 7/8/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import "FBUYourTurnViewController.h"
+
+@implementation FBUYourTurnViewController
+
+@end
